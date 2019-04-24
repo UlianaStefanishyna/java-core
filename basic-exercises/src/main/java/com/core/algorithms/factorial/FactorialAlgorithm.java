c2ca58c86498afc5c968dea0d9e390a96e5fdbb3
Uliana Stefanishyna <ulanka1996@gmail.com>
2019-04-24 14:18:04 +0300

    added factorial algorithm

---
+++ basic-exercises/src/main/java/com/core/algorithms/factorial/FactorialAlgorithm.java
@@ -0,0 +1,14 @@
+package com.core.algorithms.factorial;
+
+@SuppressWarnings("all")
+public class FactorialAlgorithm {
+
+    public int myFactorial(int n) {
+        if (n < 0)
+            throw new RuntimeException("Cannot find factorial of negative value");
+        else if (n == 0 || n == 1)
+            return 1;
+        else
+            return n * myFactorial(n - 1);
+    }
+}
---
+++ basic-exercises/src/test/java/com/core/algorithms/factorial/FactorialAlgorithmTest.java
@@ -0,0 +1,37 @@
+package com.core.algorithms.factorial;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FactorialAlgorithmTest {
+
+    private FactorialAlgorithm factorial;
+
+    @Before
+    public void setUp() {
+        this.factorial = new FactorialAlgorithm();
+    }
+
+    @Test
+    public void test_MyFactorial() {
+        int actualResult = this.factorial.myFactorial(4);
+        assertEquals(24, actualResult);
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void test_FactorialOfNegativeValue() {
+        this.factorial.myFactorial(-1);
+    }
+
+    @Test
+    public void test_FactorialOfZeroAndOne() {
+        int zeroFactorial = this.factorial.myFactorial(0);
+        int oneFactorial = this.factorial.myFactorial(1);
+
+        assertEquals(zeroFactorial, oneFactorial);
+        assertEquals(1, zeroFactorial);
+        assertEquals(1, oneFactorial);
+    }
+}
